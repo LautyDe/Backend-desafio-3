7efd9f5f7d0a3f8e653d03e386dfc274533e83cf
Lautaro Demonte <lauty.d.p@gmail.com>
2022-09-14 16:56:08 -0300

    agrego el txt con los productos

--- ejecutar.js
+++ ejecutar.js
@@ -1,27 +1,6 @@
 const Contenedor = require("./contenedor.js");
 const productos = new Contenedor("productos.txt");
 
-const producto1 = {
-  title: "Silla Gamer",
-  price: 60000,
-  thumbnail:
-    "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/720/526/products/diseno-sin-titulo-5121-bd311cff4d158548d516372654110148-1024-1024.jpg",
-};
-
-const producto2 = {
-  title: "Joystick PS5",
-  price: 25000,
-  thumbnail:
-    "https://images.fravega.com/f1000/a23c2e9cbe114eca833fc5f7288457fc.jpg",
-};
-
-const producto3 = {
-  title: "Auriculares PS5",
-  price: 42000,
-  thumbnail:
-    "https://arsonyb2c.vtexassets.com/arquivos/ids/348062/PS5_WHS_Pshot_A.jpg?v=637363806123470000",
-};
-
 const ejecutar = async () => {
   /* >>>> poner el nombre del producto a agregar en el metodo save <<<< */
   await productos.save(producto1);
--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 const express = require("express");
 
+const Contenedor = require("./contenedor.js");
+
+const productos = new Contenedor("productos.txt");
+
 const app = express();
 
 const PORT = 8080;
 
 const fs = require("fs");
-
-const productos = [
-  {
-    title: "Silla Gamer",
-    price: 60000,
-    thumbnail:
-      "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/720/526/products/diseno-sin-titulo-5121-bd311cff4d158548d516372654110148-1024-1024.jpg",
-    id: 1,
-  },
-  {
-    title: "Joystick PS5",
-    price: 25000,
-    thumbnail:
-      "https://images.fravega.com/f1000/a23c2e9cbe114eca833fc5f7288457fc.jpg",
-    id: 2,
-  },
-  {
-    title: "Auriculares PS5",
-    price: 42000,
-    thumbnail:
-      "https://arsonyb2c.vtexassets.com/arquivos/ids/348062/PS5_WHS_Pshot_A.jpg?v=637363806123470000",
-    id: 3,
-  },
-];
 
 const server = app.listen(PORT, () => {
   console.log(
@@ -38,30 +18,18 @@ const server = app.listen(PORT, () => {
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
 app.get("/", (req, res) => {
-  res.send({ mensaje: "Hola mundo!" });
+  res.send("<h1>Bienvenido a mi nueva entrega! 😎</h1>");
 });
 
 app.get("/productos", (req, res) => {
-  res.send("holaa");
+  const ejecutar = async () => {
+    const arrayProductos = await productos.getAll();
+    let card = ``;
+    arrayProductos.map(
+      (item) =>
+        (card += `<h1>Nombre: ${item.title}</h1>/n <h2>Precio: ${item.price}</h2>/n <img src"${item.thumbnail}">`)
+    );
+    res.send(card);
+  };
+  ejecutar();
 });
-
-const producto1 = {
-  title: "Silla Gamer",
-  price: 60000,
-  thumbnail:
-    "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/720/526/products/diseno-sin-titulo-5121-bd311cff4d158548d516372654110148-1024-1024.jpg",
-};
-
-const producto2 = {
-  title: "Joystick PS5",
-  price: 25000,
-  thumbnail:
-    "https://images.fravega.com/f1000/a23c2e9cbe114eca833fc5f7288457fc.jpg",
-};
-
-const producto3 = {
-  title: "Auriculares PS5",
-  price: 42000,
-  thumbnail:
-    "https://arsonyb2c.vtexassets.com/arquivos/ids/348062/PS5_WHS_Pshot_A.jpg?v=637363806123470000",
-};
