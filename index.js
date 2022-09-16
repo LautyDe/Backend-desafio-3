b924b98563151a8e3be2e8b0215d4a67e221e5b2
Lautaro Demonte <lauty.d.p@gmail.com>
2022-09-16 12:27:23 -0300

    agrego estilos a las cards

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const server = app.listen(PORT, () => {
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Bienvenido a mi nueva entrega! 😎</h1>");
+  res.send(
+    `<h1 style="text-align: center">Bienvenido a mi nueva entrega! 😎</h1>`
+  );
 });
 
 app.get("/productos", (req, res) => {
@@ -21,9 +23,11 @@ app.get("/productos", (req, res) => {
     let card = ``;
     arrayProductos.map(
       (item) =>
-        (card += `<h1>Nombre: ${item.title}</h1>/n <h2>Precio: ${item.price}</h2>/n <img src="${item.thumbnail}">`)
+        (card += `<div style="background-color: black; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${item.title}</h2> <h3>Precio: $${item.price}</h3> <img style="margin-bottom: 10px"height="250px" src="${item.thumbnail}"></div>`)
     );
-    res.send(card);
+    res.send(
+      `<h1 style="text-align: center">Todos los productos:</h1><section style="display: flex; justify-content: space-around">${card}</section>`
+    );
   };
   ejecutar();
 });
@@ -34,8 +38,10 @@ app.get("/random", (req, res) => {
     let numero = Math.floor(Math.random() * arrayProductos.length);
     let random = [];
     arrayProductos.map((item, index) => index === numero && random.push(item));
-    let card = `<h1>Nombre: ${random[0].title}</h1>/n <h2>Precio: ${random[0].price}</h2>/n <img src="${random[0].thumbnail}">`;
-    res.send("card");
+    let card = `<div style="background-color: black; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${random[0].title}</h2> <h3>Precio: ${random[0].price}</h3> <img style="margin-bottom: 10px"height="250px" src="${random[0].thumbnail}"></div>`;
+    res.send(
+      `<h1 style="text-align: center">Producto al azar:</h1><section style="display: flex; justify-content: space-around">${card}</section>`
+    );
   };
   ejecutar();
 });
